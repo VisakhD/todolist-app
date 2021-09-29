06e487d727cb087423da31437823408c3852cc6a
Visakh D <87391303+VisakhD@users.noreply.github.com>
2021-09-29 22:35:36 +0530

    2nd day commit

---
+++ coredata uikit/ViewControllers/ProfileViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileViewController.swift
+//  coredata uikit
+//
+//  Created by Visakh D on 29/09/21.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var nameTextFieldLabel: UILabel!
+    @IBOutlet weak var usernameTextFieldLabel: UILabel!
+    
+    @IBOutlet weak var emailTextField: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func logoutButton(_ sender: UIButton) {
+    }
+    
+}
